79b2797a84d70ec94fbc7ebe3aa26dcf7e4646fa
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2025-04-29 22:24:27 +0100

    added skeleton logic to run script

--- app/template/run.py
+++ app/template/run.py
@@ -1,13 +1,14 @@
 # Skeleton code for logic
 from settings import CelonisConnection, DataExtraction, DuplicateLogic
-from core import connect
+from core.connect import connect
 from core.extract import get_data_model_table, extract_data
+from core.upload import create_data_model_dfs_from_groups, add_dupe_group_relationships_tables_to_data_model
 from core.duplicate_checker import DuplicateChecker
 import uuid
 
 
 #Establish connection and get the datamodel
-celonis = connect.connect(CelonisConnection.CELONIS_BASE_URL, CelonisConnection.CELONIS_API_TOKEN, CelonisConnection.CELONIS_APP_KEY)
+celonis = connect(CelonisConnection.CELONIS_BASE_URL, CelonisConnection.CELONIS_API_TOKEN, CelonisConnection.CELONIS_APP_KEY)
 data_pool = celonis.data_integration.get_data_pool(CelonisConnection.DATA_POOL_ID)
 data_model = data_pool.get_data_model(celonis, CelonisConnection.DATA_MODEL_ID)
 
@@ -30,9 +31,14 @@ for pattern_name, pattern in DuplicateLogic.SEARCH_PATTERNS.items():
             found_groups.add(group)
             found_groups_with_pattern_name.add((group, pattern_name, uuid.uuid4())) #TODO ensure this is always unique as it will be object ID
             
-    
+#Upload data
+duplicate_group_df, relationship_df = create_data_model_dfs_from_groups(groups, {'object':'OBJECT_ID', 'group':'GROUP_ID'}) 
+add_dupe_group_relationships_tables_to_data_model(data_pool, relationship_df, 'r_o_Dupe_Object', duplicate_group_df, DataExtraction.GROUP_OBJECT_TABLE_NAME)
 
-    
+#Reload model
+data_model.reload()
+
+
     
     
 
