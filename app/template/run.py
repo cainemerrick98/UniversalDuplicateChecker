97cb2e4934f6970ffb2639916a18d6289adf9ea0
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2025-04-29 21:20:37 +0100

    updated run script to use pycelonis methods to get data integrations objects

--- app/template/run.py
+++ app/template/run.py
@@ -7,7 +7,8 @@ from pycelonis.config import Config
 
 #Establish connection and get the datamodel
 celonis = connect.connect(CelonisConnection.CELONIS_BASE_URL, CelonisConnection.CELONIS_API_TOKEN, CelonisConnection.CELONIS_APP_KEY)
-data_model = connect.get_data_model(celonis, CelonisConnection.DATA_POOL_ID, CelonisConnection.DATA_MODEL_ID)
+data_pool = celonis.data_integration.get_data_pool(CelonisConnection.DATA_POOL_ID)
+data_model = data_pool.get_data_model(celonis, CelonisConnection.DATA_MODEL_ID)
 
 #Get the relevant object tables
 group_object_table = get_data_model_table(data_model, DataExtraction.GROUP_OBJECT_TABLE_NAME)
