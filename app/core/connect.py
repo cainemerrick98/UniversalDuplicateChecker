9c515794e41dad5666bbc264fac5c05deae877e8
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2025-04-29 21:19:17 +0100

    removed get data model function

--- app/core/connect.py
+++ app/core/connect.py
@@ -5,10 +5,3 @@ from pycelonis.ems.data_integration.data_model import DataModel
 def connect(base_url:str, api_token:str, app_key:str)->Celonis:
     celonis = get_celonis(base_url, api_token, app_key)
     return celonis
-
-def get_data_model(celonis:Celonis, data_pool_id:str, data_model_id:str) -> DataModel:
-    return celonis.data_integration.get_data_pool(
-        data_pool_id
-    ).get_data_model(
-        data_model_id
-    )
